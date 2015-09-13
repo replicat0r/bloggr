5f187a7d3deeefb9ae9f5d9f450c5c568ad09f99
tyler liu <replicat0r.misc@gmail.com>
2015-09-13 18:51:09 -0400

    added validation for restricted and invalid name for subdomains

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -3,5 +3,12 @@ module Subscribem
   	belongs_to :owner, :class_name => "Subscribem::User"
   	accepts_nested_attributes_for :owner
   	validates :subdomain, :presence => true, :uniqueness => true
+  	before_validation do
+  		self.subdomain = subdomain.to_s.downcase
+  	end
+  	EXCLUDED_SUBDOMAINS = %w(admin)
+  	validates_exclusion_of :subdomain, :in => EXCLUDED_SUBDOMAINS, 
+  		:message => "is not allowed. Please choose another domain."
+  	validates_format_of :subdomain, :with => /\A[\w\-]+\Z/i, :message => "is not allowed. Please choose another domain"
   end
 end
