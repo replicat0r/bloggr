5c5c3b18a5d9154042b91b3d9c0a51c92c661ed1
tyler liu <replicat0r.misc@gmail.com>
2015-09-15 16:23:21 -0400

    finished user sign up under subdomain

--- app/controllers/subscribem/application_controller.rb
+++ app/controllers/subscribem/application_controller.rb
@@ -1,28 +1,33 @@
 module Subscribem
   class ApplicationController < ActionController::Base
-  	def current_account
-  		if user_signed_in?
-  			current_account ||= env["warden"].user(:scope => :account)
-  		end
-  	end
+    def current_account
+      if user_signed_in?
+        current_account ||= env["warden"].user(:scope => :account)
+      end
+    end
 
-  	def current_user
-			if user_signed_in?
-				current_user ||= env["warden"].user(:scope => :user)
-			end
-	  end
+    def current_user
+      if user_signed_in?
+        current_user ||= env["warden"].user(:scope => :user)
+      end
+    end
 
-	  def user_signed_in?
-	  	env["warden"].authenticated?(:user)
-	  end
+    def user_signed_in?
+      env["warden"].authenticated?(:user)
+    end
 
-	  def authenticate_user!
-	  	unless user_signed_in?
-	  		flash[:notice] = "Please sign in."
-	  		redirect_to "/sign_in"
-	  	end
-	  end
+    def authenticate_user!
+      unless user_signed_in?
+        flash[:notice] = "Please sign in."
+        redirect_to "/sign_in"
+      end
+    end
 
-	  helper_method :user_signed_in? , :current_user, :current_account
-	end	
+    def force_authentication!(account, user)
+      env["warden"].set_user(user, :scope => :user)
+      env["warden"].set_user(account, :scope => :account)
+    end
+
+    helper_method :user_signed_in? , :current_user, :current_account
+  end
 end
